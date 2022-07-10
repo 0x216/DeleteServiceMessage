763a69697fe47e26fd9c605430c9e06722656d4c
0x216 <89255070+0x216@users.noreply.github.com>
2022-07-10 19:06:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ async def pmfilter(client, message):
     me = await message._client.get_me()
     await message.reply_text( f"""<b>👋🏻 Hi {message.from_user.first_name}!</b>
 I am Delete Events bot, I can delete Service message. Just Add me in group as admin.""",
-                             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="ðŸ”¶ ADD Me ðŸ”¶",url=f"https://t.me/Del_Events_Bot?startgroup=true")]]),
+                             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Add me to the group",url=f"https://t.me/EuroGroupService_bot?startgroup=true")]]),
                              quote=True)
 
 @Delevents.on_message(filters.private & ~filters.command('start'))
